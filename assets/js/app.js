4d3aed6cf4efc9a86961a0342236f06c21bf2d41
ImDamianCortes <dcortesmunoz@gmail.com>
2022-03-17 01:01:08 -0300

    primer ingreso

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// Argumentos
+
+a = "Cotizacion"    // argumento[0]
+b = "txt"           // argumento[1]
+c = "Dolar"         // argumento[2]
+d = 250000          // argumento[3]
+
+var exec = require('child_process').exec
+
+var modulo = `node main.js ${a} ${b} ${c} ${d}`
+
+// donde se ejecuta con node main.js
+exec(`${modulo}`, function (err, stdout, stderr) {
+  console.log(stdout)
+})
